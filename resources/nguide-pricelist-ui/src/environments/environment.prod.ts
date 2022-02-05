9fab8cffc63fe710a0cebd73185ecdda5de24b77
andreiandruh <andrei.andruh@nguide.ro>
2022-02-05 19:33:04 +0200

    fixed ui

--- resources/nguide-pricelist-ui/src/environments/environment.prod.ts
+++ resources/nguide-pricelist-ui/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiBaseURL: 'http://127.0.0.1:8000/api/',
+  apiBaseURL: '/api/',
   defaultLocale: 'nl',
   filePublicApiURL: '/imgc/',
   fileUploadApiURL: '/api/files'
